9d4e34e01bf79eafe5bb8a9a5cc32dfabc2778b3
ahmadyassinkhoja <43853262+ahmadyassinkhoja@users.noreply.github.com>
2019-03-11 13:02:47 +0200

    added cart icon with badge

--- App.js
+++ App.js
@@ -1,42 +1,74 @@
 import React from 'react'
-import { StyleSheet, View, ScrollView } from 'react-native'
+import { StyleSheet, View, ScrollView, Text } from 'react-native'
 
 import { Constants } from 'expo'
 
-import { Button, ThemeProvider, Header } from 'react-native-elements'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { Button, ThemeProvider, Header, Badge, withBadge, Icon  } from 'react-native-elements'
 
-import { createAppContainer, createStackNavigator, StackActions, NavigationActions } from 'react-navigation'
+import {
+    createAppContainer,
+    createStackNavigator,
+    StackActions,
+    NavigationActions
+} from 'react-navigation'
 
 import Products from './components/Products'
 
-class App extends React.Component {
+import IconBadge from 'react-native-icon-badge'
 
+
+const CartIcon = () => (
+    <View style={{flexDirection: 'row',alignItems: 'center',justifyContent: 'center'}}>
+        <IconBadge
+            MainElement={
+                <View style={{ width:50, height:50, marginTop: 23}}>
+                    <Icon name='shopping-cart' color='#fff' style={{ width:70, height:70 }}/>
+                </View>
+            }
+            BadgeElement={
+                <Text style={{color:'#FFFFFF'}}> 1 </Text>
+            }
+            IconBadgeStyle={
+                {   
+                    width:20,
+                    height:20,
+                    backgroundColor: 'red',
+                    marginTop: 15
+                }
+            }
+        />
+    </View>
+)
+
+
+class App extends React.Component {
     render() {
         return (
-            <View style={{marginBottom: 20}}>
-                <ScrollView>
-                    <Header
-                        // leftComponent={{ icon: 'menu', color: '#fff' }}
-                        centerComponent={{ text: 'Computer Store', style: { color: '#fff' } }}
-                        // rightComponent={{ icon: 'home', color: '#fff' }}
-                    />
+            <ScrollView>
+                <Header
+                    centerComponent={{
+                        text: 'Computer Store',
+                        style: { color: '#fff' }
+                    }}
+                    rightComponent={<CartIcon/> }
+                />
 
-                    <Products/>
-                </ScrollView>
-            </View>
+                <Products />
+            </ScrollView>
         )
     }
 }
 
-
-const AppNavigator = createStackNavigator({
-    Home: {
-        screen: App,
+const AppNavigator = createStackNavigator(
+    {
+        Home: {
+            screen: App
+        }
     },
-}, {
-    initialRouteName: 'Home',
-    headerMode: 'none'
-})
-  
+    {
+        initialRouteName: 'Home',
+        headerMode: 'none'
+    }
+)
+
 export default createAppContainer(AppNavigator)
